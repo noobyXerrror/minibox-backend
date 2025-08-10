9f0c8144a28d76813caf1ef9361a081b4163243a
noobyXerrror <97517177+noobyXerrror@users.noreply.github.com>
2025-08-10 13:15:56 +0530

    allowerd cors for render.com

--- config/settings.py
+++ config/settings.py
@@ -176,6 +176,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    'minibox-backend.onrender.com',
 ]
 
 AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend', 
